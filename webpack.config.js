bd80fedb61175a7d00f720d9944564a61f8889c0
Elizabeth Rivera <elizabethriver@gmail.com>
2021-02-04 16:21:47 -0500

    cambios

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
 
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
 
-    output: {
-        filename: 'app.bundle.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ],
-    devServer: {
-        open:true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/, 
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
+  output: {
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
 
-                }
-            }
-        ]
-    }
+        }
+      }
+    ]
+  }
 
 }
