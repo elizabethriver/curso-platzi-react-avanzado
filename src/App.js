6301f11130d81fdc48878cc7f6808468af4a89c7
Elizabeth Rivera <elizabethriver@gmail.com>
2021-02-05 16:34:57 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ListOfCategory } from './components/ListOfCategories'
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 import { PhotoCard } from './components/PhotoCard'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 import { Logo } from './components/Logo'
