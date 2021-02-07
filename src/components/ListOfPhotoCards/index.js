62b1ff374917ea5c763cce1d54efd90073a79d61
Elizabeth Rivera <elizabethriver@gmail.com>
2021-02-07 17:32:53 -0500

    comit

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -5,8 +5,8 @@ import { useQuery, gql } from '@apollo/client'
 // import { List, Item } from './style'
 
 const withPhotos = gql`
-query getPhotos {
-  photos {
+query getPhotos ($categoryId: ID) {
+  photos (categoryId: $categoryId) {
     id
     categoryId
     src
